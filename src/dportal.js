bef57b46e0527f308e8165708ec64c8bfa70b807
Towey <8003376@qq.com>
2024-02-23 16:20:36 +0800

    优化启动文件

--- src/dportal.js
+++ src/dportal.js
@@ -1,13 +1,54 @@
 import Koa from 'koa';
+import got, { Options } from 'got';
+import fs from 'fs';
+import compressing from 'compressing';
 import staticMiddleware from 'koa-static';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const urlFile = "https://test-lonchbi.lonch.com.cn/devPortal.zip";
+const temFile = join(__dirname, './devPortal.zip');
 const app = new Koa();
-const staticMiddleware1 = staticMiddleware(join(__dirname, './devPortal'));
-app.use(staticMiddleware1);
+downLoadFile().then(()=>{
+  compressing.zip.uncompress(temFile, join(process.env.USERPROFILE, './'))
+  .then(serveRun)
+  .catch(()=>{});
+});
+
+function downLoadFile(){
+  let isCloseRun = false;
+  const writableStream = fs.createWriteStream(temFile);
+  return new Promise((resolve, reject)=>{
+    const options = new Options({
+      isStream: true,
+    });
+    const stream = got.get(urlFile, options)
+    stream.on('error',()=>{
+      isCloseRun = true;
+    })
+    stream.on('data', (chunk)=>{
+      writableStream.write(chunk);
+    });
+    stream.on('end', async ()=>{
+      writableStream.end();
+      if(!isCloseRun){
+        resolve();
+      }
+    });
+  })
+}
+
+function serveRun(){
+  // 删除下载文件
+  fs.unlink(temFile,(err)=>{
+    if (err) throw err;
+  });
+  const staticMiddleware1 = staticMiddleware(join(process.env.USERPROFILE, './devPortal'));
+  app.use(staticMiddleware1);
+}
+
 app.listen(8080, () => {
   console.log("Server started on port 8080");
 });
